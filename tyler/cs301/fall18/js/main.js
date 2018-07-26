abc45dfbac8cf09026277cc1e92ffc0f0a2be2d6
tylerharter <tylerharter@gmail.com>
2018-07-26 11:12:47 -0500

    try to remove flicker

--- tyler/cs301/fall18/js/main.js
+++ tyler/cs301/fall18/js/main.js
@@ -1,13 +1,20 @@
-$(function() {
+(function() {
     function main() {
 	get_nav()
     }
 
     function get_nav() {
-	$("#navbarCollapse").load("nav.html", got_nav);
+	var nav = document.getElementById("navbarCollapse");
+	nav.innerHTML = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+	console.log(nav)
+	jQuery.get('nav.html', got_nav);
     }
 
-    function got_nav() {
+    function got_nav(html) {
+	var nav = document.getElementById("navbarCollapse");
+	nav.innerHTML = html
+
+	// highlight current page
 	var curr_page = window.location.pathname.split('/').slice(-1)[0]
 	$('.nav-link').each(function(i,link) {
 	    var curr_link = link.href.split('/').slice(-1)[0]
@@ -19,4 +26,4 @@ $(function() {
     }
 
     main()
-});
+})()
