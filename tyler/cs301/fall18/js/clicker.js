59a5f0b1d1081021f40cff3d4fb5b6d000168ae7
tylerharter <tylerharter@gmail.com>
2018-08-06 12:51:18 -0500

    clicker+common js files

---
+++ tyler/cs301/fall18/js/clicker.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var clicker = {};
+
+$(function() {
+    var currQuestionId = null
+
+    clicker.clickerSubmit = function(answer) {
+	var data = {
+	    "fn": "answer",
+	    "question_id": currQuestionId,
+	    "answer": answer
+	}
+	callLambda(data, function(data) {
+	    console.log("answer uploaded")
+	})
+    };
+
+    clicker.clickerSummarizeAnswers = function() {
+	var data = {
+	    "GoogleToken": googleToken,
+	    "fn": "get_answer_counts",
+	}
+	callLambda(data, function(data) {
+	    var answers = data.body.answers
+	    var total = 0
+	    for(var k in answers) {
+		total += answers[k]
+	    }
+
+	    var text = ""
+	    Object.keys(answers).sort().forEach(function(k){
+		var count = answers[k]
+		text += (k + ": " + count + " (" + Math.round(count*100/total) + "%)\n")
+	    })
+	    $("#answers").val(text)
+
+	    if (data.body.errors.length > 0) {
+		console.log("Errors:")
+		console.log(data.body.errors)
+	    }
+	})
+    };
+
+    clicker.clickerRefreshQuestion = function() {
+	var data = {
+	    "GoogleToken": googleToken,
+	    "fn": "get_question"
+	}
+	callLambda(data, function(data) {
+	    $("#question").val(data.body.question)
+	    currQuestionId = data.body.id
+	})
+    };
+
+    clicker.clickerUploadQuestion = function() {
+	console.log($("#question").val())
+	var data = {
+	    "GoogleToken": googleToken,
+	    "fn": "put_question",
+	    "question": $("#question").val()
+	}
+
+	callLambda(data, function(data) {
+	    console.log("question uploaded")
+	})
+    };
+
+    main()
+})
