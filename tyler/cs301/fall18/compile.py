9ddab32484a668fa29211d0c9a6e3d26de8e9661
Adalbert Gerald Soosai Raj <adalbert.gerald@gmail.com>
2018-09-05 11:31:57 -0500

    Section 3 schedule/syllabus added.

--- tyler/cs301/fall18/compile.py
+++ tyler/cs301/fall18/compile.py
@@ -97,8 +97,70 @@ def schedule():
 
     f.close()
 
+def schedule3():
+    with open('schedule3.txt') as f:
+        days = list(map(format_day, f.read().split('=\n')))
+        
+    f = open('schedule3.content.html', 'w', encoding='utf-8')
+    f.write('<h1 class="mt-5">Lecture Schedule</h1>\n')
+    
+    start_date = date(2018, 9, 4)
+    end_date = date(2018, 12, 11)
+    day_count = (end_date - start_date).days + 1
+
+    cells = []
+    full = 0
+    free = 0
+    for curr in (start_date + timedelta(n) for n in range(day_count)):
+        day = calendar.day_abbr[curr.weekday()]
+        if day in ['Tue', 'Thu']:
+            if len(days) == 0:
+                free += 1
+            else:
+                full += 1
+            content = days.pop(0) if len(days)>0 else 'TBD\n...'
+            title,content = content[:content.index('\n')], content[content.index('\n')+1:]
+            header = '<h5><strong>%s</strong>: %s (%s)</h5>' % (curr.strftime("%a"),
+                                               title,
+                                               curr.strftime("%b %-d"))
+            cells.append('%s\n%s\n' % (header, content))
+    print('%d free days, %d full days' % (free, full))
+
+    with open('schedule.json') as extra_sched:
+        extra = json.loads(extra_sched.read())
+    events = extra['events'] # indexed by week
+    sections = extra['sections'] # indexed by week
+    
+    # dump cells
+    cols = 2
+    for i in range(0, len(cells), cols):
+        week = i//cols+1
+
+        # sections
+        for section in sections.get(str(week), []):
+            f.write('<div class="alert alert-primary my-5 week">%s</div>\n' % section)
+
+        # days
+        row = cells[i:i+cols]
+        f.write('<h3 class="my-3 border-bottom border-top">Week %d</h2>\n' % week)
+        
+        f.write('<div class="row">\n')
+        for cell in row:
+            f.write('<div class="col-md-4 my-3">\n')
+            f.write(cell)
+            f.write('</div>\n')
+        f.write('</div>\n')
+
+        # warnings
+        for event in events.get(str(week), []):
+            f.write('<div class="alert alert-danger my-2">%s</div>\n' % event)
+    f.write('<br>')
+
+    f.close()
+
 def main():
     schedule()
+    schedule3()
     template()
 
 if __name__ == '__main__':
