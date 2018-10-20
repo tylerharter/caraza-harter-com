ae00546e5a7d167018e95f12793610917b9edd27
tylerharter <tylerharter@gmail.com>
2018-10-20 11:10:32 -0500

    parallel backup

--- tools/s3_backup.py
+++ tools/s3_backup.py
@@ -1,12 +1,14 @@
 import boto3, botocore, base64, json, os, sys
+from multiprocessing import Pool
 
 BUCKET = 'caraza-harter-cs301'
-session = boto3.Session(profile_name='cs301ta')
-s3 = session.client('s3')
 
 # return all S3 objects with the given key prefix, using as many
 # requests as necessary
 def s3_all_keys(Prefix):
+    session = boto3.Session(profile_name='cs301ta')
+    s3 = session.client('s3')
+
     ls = s3.list_objects_v2(Bucket=BUCKET, Prefix=Prefix, MaxKeys=10000)
     keys = []
     while True:
@@ -19,41 +21,48 @@ def s3_all_keys(Prefix):
                                 MaxKeys=10000)
     return keys
 
-def download_submission(project_path):
-    # a project path will look something like this:
-    # projects/p0/users/115799594197844895033/curr.json
+def chunks(l):
+    n = 250
+    c = []
+    for i in range(0, len(l), n):
+        c.append(l[i:i + n])
+    return c
 
-    user_id = project_path.split('/')[-2]
-    net_id = lookup_net_id(user_id)
-    if net_id == None:
-        print('could not lookup NetID for google user_id='+user_id)
-        return
+def download_chunk(paths):
+    session = boto3.Session(profile_name='cs301ta')
+    s3 = session.client('s3')
 
-    # create user dir for download
-    user_dir = os.path.join(SUBMISSIONS, net_id)
-    print('download to ' + user_dir)
-    if not os.path.exists(user_dir):
-        os.mkdir(user_dir)
-
-    # download
-    response = s3.get_object(Bucket=BUCKET, Key=project_path)
-    submission = json.loads(response['Body'].read().decode('utf-8'))
-    file_contents = base64.b64decode(submission.pop('payload'))
-    with open(os.path.join(user_dir, submission['filename']), 'wb') as f:
-        f.write(file_contents)
-    with open(os.path.join(user_dir, 'meta.json'), 'w') as f:
-        f.write(json.dumps(submission, indent=2))
+    try:
+        for path in paths:
+            print('File {}'.format(path))
+            local = 'psnapshot/' + path
+            if os.path.exists(local):
+                continue
+            response = s3.get_object(Bucket=BUCKET, Key=path)
+            os.makedirs(os.path.dirname(local), exist_ok=True)
+            with open(local, 'wb') as f:
+                f.write(response['Body'].read())
+    except Exception as e:
+        return e
+    return None
 
 def main():
-    for i,path in enumerate(s3_all_keys("")):
-        print('File {}'.format(i))
-        local = 'snapshot/' + path
-        if os.path.exists(local):
-            continue
-        response = s3.get_object(Bucket=BUCKET, Key=path)
-        os.makedirs(os.path.dirname(local), exist_ok=True)
-        with open(local, 'wb') as f:
-            f.write(response['Body'].read())
+    print('lookup keys')
+    paths = s3_all_keys("")
+    print('chunk keys')
+    paths = chunks(paths)
+
+    print('parallel map')
+    with Pool(20) as p:
+        errors = p.map(download_chunk, paths)
+
+    errors = [e for e in errors if e != None]
+    if len(errors) == 0:
+        print('no errors')
+    else:
+        print('ERRORS:')
+        for e in errors:
+            print(e)
 
 if __name__ == '__main__':
     main()
