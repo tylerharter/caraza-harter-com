83d521a7e23dfb2e317bd19d0e224d3aee1d93fa
tylerharter <tylerharter@gmail.com>
2019-02-06 11:30:33 -0600

    lab swap

--- tools/lab-swap.py
+++ tools/lab-swap.py
@@ -21,32 +21,38 @@ class Swapper:
         self.reqs = reqs
 
     def swap(self):
-        diff = ddict(int)
         swaps = []
 
-        diff['Thu @ 9:30am'] = 
-        diff['Thu @ 11am'] = 
-        diff['Thu @ 1pm'] = 
-        diff['Thu @ 2:30pm'] = 
-        diff['Thu @ 4pm'] = 
-        
-        diff['Fri @ 9:30am'] = 
-        diff['Fri @ 11am'] = 
-        diff['Fri @ 1pm'] = 
-        diff['Fri @ 2:30pm'] = 
-        diff['Fri @ 4pm'] = 
+        enrolled = {}
+        enrolled['Thu @ 9:30am'] = 48
+        enrolled['Thu @ 11am'] = 52
+        enrolled['Thu @ 1pm'] = 52
+        enrolled['Thu @ 2:30pm'] = 51
+        enrolled['Thu @ 4pm'] = 50
+        enrolled['Fri @ 9:30am'] = 49
+        enrolled['Fri @ 11am'] = 48
+        enrolled['Fri @ 1pm'] = 52
+        enrolled['Fri @ 2:30pm'] = 39
+        enrolled['Fri @ 4pm'] = 41
+
+        with open('/Users/trh/git_co/cs301/s19/lab-swaps/lab-swap.json') as f:
+            for row in json.load(f):
+                if row.get("status", "") == "rejected":
+                    continue
+                enrolled[row["from"]] -= 1
+                enrolled[row["to"]] += 1
 
         random.shuffle(self.reqs)
         for req in self.reqs:
             # find target with least enrollment
             dst = req['to'][0]
             for dst2 in req['to'][1:]:
-                if diff[dst2] < diff[dst]:
+                if enrolled[dst2] < enrolled[dst]:
                     dst = dst2
 
-            if diff[dst] < 4:
-                diff[dst] += 1
-                diff[req['from']] -= 1
+            if enrolled[dst] < 54:
+                enrolled[dst] += 1
+                enrolled[req['from']] -= 1
                 swaps.append({
                     "email": req["Email Address"],
                     "from": req["from"],
@@ -55,8 +61,8 @@ class Swapper:
             else:
                 print('fail:', req["Email Address"], req["from"], req["to"])
 
-        for time in sorted(diff.keys()):
-            print(time, diff[time])
+        for time in sorted(enrolled.keys()):
+            print(time, enrolled[time])
 
         return swaps
 
