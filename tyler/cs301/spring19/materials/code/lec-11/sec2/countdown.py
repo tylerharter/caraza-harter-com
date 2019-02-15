ec3e5e19ac901a0b5592435262252e015ce50538
tylerharter <tylerharter@gmail.com>
2019-02-15 14:51:28 -0600

    lec 11 materials

---
+++ tyler/cs301/spring19/materials/code/lec-11/sec2/countdown.py
@@ -0,0 +1,17 @@
+import time
+import beeper
+
+seconds = input("how many seconds? [q for quit] ")
+
+while seconds != "q":
+    seconds = int(seconds)
+
+    while seconds > 0:
+        print(seconds, "remaining")
+        time.sleep(1)
+        seconds -= 1
+            
+    print("beep beep beep")
+    beeper.beep(3)
+
+    seconds = input("how many seconds? [q for quit] ")
