9e3a52631ed2aee0387d83e111ea0d87741e07f3
tylerharter <tylerharter@gmail.com>
2019-03-26 20:55:12 -0500

    lecture 25 interactive

---
+++ tyler/cs301/spring19/materials/lec-25/p1.py
@@ -0,0 +1,8 @@
+try:
+    print("2 inverse is", 1/2)
+    print("1 inverse is", 1/1)
+    print("0 inverse is", 1/0)
+    print("-1 inverse is", -1/1)
+    print("-2 inverse is", -1/1)
+except:
+    print("that's all, folks!")
