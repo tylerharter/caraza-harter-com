95a1a87cc49ea0d4f8601f941750c19d37a78d1f
tylerharter <tylerharter@gmail.com>
2019-10-16 09:26:46 -0500

    lec code

---
+++ tyler/cs301/fall19/materials/code/lec-19/lec1/process_csv.py
@@ -0,0 +1,9 @@
+import csv
+
+# copied from https://automatetheboringstuff.com/chapter14/
+def process_csv(filename):
+    exampleFile = open(filename, encoding="utf-8")
+    exampleReader = csv.reader(exampleFile)
+    exampleData = list(exampleReader)
+    exampleFile.close()
+    return exampleData
