8e5b43754c3ace0cb882e7559ff8be24be347c3b
tylerharter <tylerharter@gmail.com>
2019-10-17 18:40:08 -0500

    lec 20 worksheet

--- tools/assign-crs.py
+++ tools/assign-crs.py
@@ -6,15 +6,14 @@ from collections import defaultdict as ddict
 # aws s3 cp p2-cr-assignments.json s3://caraza-harter-cs301/a/projects/p2-cr-assignments.json
 
 COURSE = 'a'
-PROJECT = 'p5'
 
 # we want submissions that are the latest for a student
-def get_submission_emails():
+def get_submission_emails(project):
     # key is user email
     links = ddict(list)
     submissions = ddict(list)
 
-    for d, _, file_paths in os.walk('snapshot/%s/projects/%s' % (COURSE, PROJECT)):
+    for d, _, file_paths in os.walk('snapshot/%s/projects/%s' % (COURSE, project)):
         for path in file_paths:
             path = os.path.join(d, path)
             email = path.split(os.sep)[4].replace('*at*', '@')
@@ -31,7 +30,12 @@ def get_submission_emails():
 
 
 def main():
-    submitters = sorted(get_submission_emails())
+    if len(sys.argv) != 2:
+        print("Usage: python3 assign-crs.py <PROJECT>")
+        return
+    project = sys.argv[1]
+    
+    submitters = sorted(get_submission_emails(project))
     with open('roster.json') as f:
         emails = {row['net_id']+'@wisc.edu' for row in json.load(f) if row['enrolled']}
     submitters = emails & set(submitters)
@@ -51,8 +55,11 @@ def main():
     assignments = {}
     for i, student in enumerate(students):
         assignments[student] = tas[i % len(tas)]
-    with open('%s-cr-assignments.json' % PROJECT, 'w') as f:
+    with open('%s-cr-assignments.json' % project, 'w') as f:
         json.dump(assignments, f, indent=2)
+
+    print('Run this:')
+    print('aws s3 cp %s-cr-assignments.json s3://caraza-harter-cs301/a/projects/%s-cr-assignments.json' % (project, project))
 
 
 if __name__ == '__main__':
