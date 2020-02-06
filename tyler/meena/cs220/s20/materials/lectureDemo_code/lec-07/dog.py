97dddfb5835af74c3c72f59d04f5454b2366da6f
Meenakshi Syamkumar <ms@cs.wisc.edu>
2020-02-05 18:06:34 -0600

    Adding code from LEC-07 demo

--- tyler/meena/cs220/s20/materials/lectureDemo_code/lec-07/dog.py
+++ tyler/meena/cs220/s20/materials/lectureDemo_code/lec-07/dog.py
@@ -1,5 +1,9 @@
+# write a comment here
 def speak():
+    "this makes the dog bark a LOT"
     print("BARK " * 20)
+    #Implicit return statement
+    return None
 
 def fetch():
     return "stick"
