c4fc09f653dd174bc8a8b623213a52b6761667ea
Meenakshi Syamkumar <ms@cs.wisc.edu>
2020-02-18 22:27:11 -0600

    Adding code for lec-08 and lec-09

---
+++ tyler/meena/cs220/s20/materials/lectureDemo_code/lec-08/battleship.py
@@ -0,0 +1,34 @@
+width = 10
+height = 8
+
+ship_x = 7
+ship_y = 3
+
+#Keep track of total score
+totalScore = 0
+
+def playBattleShip():
+    #Use a global variable to track score across multiple function calls
+    global totalScore
+
+    guess_x = int(input("Enter guess x: "))
+    guess_y = int(input("Enter guess y: "))
+
+    # STEP 1: rows before guess
+    print(("." * width + "\n") * guess_y, end="")
+
+    # STEP 2: guess row
+    print("." * guess_x, end="")
+    symbol = int(guess_x == ship_x and guess_y == ship_y)
+    print(symbol, end="")
+    print("." * (width - guess_x - 1))
+
+    # STEP 3: rows after guess
+    print(("." * width + "\n") * (height - guess_y - 1))
+
+    totalScore += symbol
+
+playBattleShip()
+playBattleShip()
+playBattleShip()
+print("Total score is:", totalScore)
