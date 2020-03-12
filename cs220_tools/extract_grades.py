468b6da2ed0338653ea246ab19ec9fe3046582d0
Meenakshi Syamkumar <ms@cs.wisc.edu>
2020-03-12 00:25:53 -0500

    Adding a copy of CS220 tools for late days message generation

---
+++ cs220_tools/extract_grades.py
@@ -0,0 +1,149 @@
+import os, sys, json, copy, math
+from collections import defaultdict as ddict
+
+COURSE = 'a'
+not_reviewed = {'p1'}
+#CR_URL = 'https://tyler.caraza-harter.com/cs301/fall19/code_review.html?project_id=%s&student_email=%s@wisc.edu'
+CR_URL = 'https://www.msyamkumar.com/cs220/s20/code_review.html?project_id=%s&student_email=%s@wisc.edu'
+
+def try_read_json(path, default={}):
+    try:
+        if "rcody3" in path:
+            print(path)
+        with open(path) as f:
+            return json.load(f)
+    except Exception as e:
+        if "rcody3" in path:
+            print("DEFAULT:", end="")
+            print(default)
+            print(e)
+        return copy.deepcopy(default)
+
+
+class Grades:
+    def __init__(self, project):
+        self.project = project
+        with open('roster.json') as f:
+            net_ids = {row['net_id'] for row in json.load(f) if row['enrolled']}
+
+        self.extensions = self.get_extensions()
+        self.grade_rows = [self.get_student_grade(nid) for nid in net_ids]
+
+
+    # returns dict
+    # key: submission file path
+    # val: days extended
+    def get_extensions(self):
+        extensions = ddict(int)
+
+        dirname = 'snapshot/%s/extensions/%s/' % (COURSE, self.project)
+        if not os.path.exists(dirname):
+            return extensions
+        for name in os.listdir(dirname):
+            if not name.endswith(".json"):
+                continue
+            path = os.path.join(dirname, name)
+            with open(path) as f:
+                days = json.load(f)["days"]
+            net_id = name.split("*at*")[0]
+            links = self.get_proj_links(net_id)
+            for link in links:
+                with open(link) as f:
+                    sub = json.load(f)['symlink']
+                    extensions[sub] = max(extensions[sub], days)
+        return extensions
+
+
+    def dump(self, path):
+        with open(path, "w") as f:
+            f.write('[\n')
+            for i, row in enumerate(self.grade_rows):
+                json.dump(row, f)
+                if i < len(self.grade_rows) - 1:
+                    f.write(",")
+                f.write("\n")
+            f.write(']\n')
+
+
+    def get_proj_links(self, net_id):
+        dirname = 'snapshot/%s/projects/%s/%s*at*wisc.edu' % (COURSE, self.project, net_id)
+        link = None
+        if os.path.exists(dirname):
+            return [os.path.join(dirname, p) for p in os.listdir(dirname) if p.endswith('-link.json')]
+        return []
+
+
+    def get_student_grade(self, net_id):
+        grade = None
+        links = sorted(self.get_proj_links(net_id))
+        for i, link in enumerate(links):
+            with open(link) as f:
+                submission_dir = json.load(f)['symlink']
+            grade2 = self.get_submission_grade(net_id, submission_dir)
+            grade2["latest_submission"] = (i == len(links) - 1)
+            # only take a later submission over a prior one if the
+            # score is better, because the earlier one will incur
+            # fewer late days
+            if grade == None or grade2["score"] > grade["score"]:
+                grade = grade2
+        else:
+            submission_dir = None
+
+        if grade == None:
+            grade = self.get_submission_grade(net_id, None)
+
+        return grade
+
+
+    def get_submission_grade(self, net_id, submission_dir):
+        # we need to check four files:
+        # - tests
+        # - CR
+        # - submission (to determine lateness)
+        # - extensions (to adjust lateness)
+
+        ready = False
+        late_days_ungraded_project = 0
+        if submission_dir:
+            sub = try_read_json(os.path.join("snapshot", COURSE, submission_dir, "submission.json"), {})
+            test = try_read_json(os.path.join("snapshot", COURSE, submission_dir, "test.json"), {})
+            cr = try_read_json(os.path.join("snapshot", COURSE, submission_dir, "cr.json"), {})
+            if net_id == "rcody3":
+                print(os.path.join("snapshot", COURSE, submission_dir, "test.json"))
+                print(test)
+                print(cr)
+            if sub != {} and test != {} and (cr != {} or self.project in not_reviewed):
+                test_score = test.get("score", 0)
+                ta_deduction = cr.get("points_deducted", 0)
+                late_days = math.ceil(max(sub.get("late_days", 0) - self.extensions[submission_dir], 0))
+                comment_count = 0
+                for comments in cr.get("highlights", {}).values():
+                    comment_count += len(comments)
+                ready = True
+            elif sub != {}:
+                late_days_ungraded_project  = math.ceil(max(sub.get("late_days", 0) - self.extensions[submission_dir], 0))
+
+        if not ready:
+            test_score = 0
+            ta_deduction = 0
+            late_days = late_days_ungraded_project
+            #late_days = 0
+            comment_count = 0
+
+        score = max(test_score - ta_deduction, 0)
+        cr_url = CR_URL % (self.project, net_id)
+
+        return {"project": self.project, "net_id": net_id, "test_score": test_score,
+                "ta_deduction": ta_deduction, "score": score, "late_days": late_days,
+                "comment_count": comment_count, "ready": ready, "code_review_url": cr_url}
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 extract_grades.py p1 [p2 ...]")
+    for project in sys.argv[1:]:
+        Grades(project).dump(os.path.join('grades', project+'.json'))
+
+
+if __name__ == '__main__':
+     main()
---
+++ cs220_tools/share_project_summary.py
@@ -0,0 +1,81 @@
+import os, sys, json, math, datetime, boto3
+from collections import defaultdict as ddict
+
+BUCKET = 'caraza-harter-cs301'
+s3 = boto3.client('s3')
+
+TEST_NET_IDS = []
+
+LATE_DAY_ALLOCATION = 9
+
+def gen_html(prows, include_intro=True):
+    cum_late = 0
+
+    intro = """<p>
+    Dear Student,
+    </p>
+
+    <p>Here is your project summary, as of Mar 12 2020:</p>
+    """
+
+    if include_intro:
+        html = intro.split('\n')
+    else:
+        html = ['<h1>']
+
+    for p in prows:
+        html.append('<h2>' + p['project'].upper() + '</h2>')
+        html.append('<ul>')
+        line = '<li>Feedback: <a href="{}">{} reviewer comments</a>'
+        html.append(line.format(p['code_review_url'], p['comment_count']))
+        html.append('<li>Score: ' + str(p['score']))
+        if p.get('override', False):
+            html[-1] += ' [override]'
+        html.append('<li>Days Late: ' + str(p['late_days']))
+
+        # handle late days
+        cum_late += p['late_days']
+        if (cum_late > LATE_DAY_ALLOCATION and p['late_days'] > 0):
+            print('dropping projects because late!')
+            html.append("<li>WARNING: late days exhausted ({} used to this point). ".format(cum_late) +
+                        "This won't be counted. "+
+                        "Please email your instructor to discuss.")
+
+        html.append('</ul>')
+    return '\n'.join(html)
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: python share_project_summary.py <project_id1> [<project_id2> ...]')
+        sys.exit(1)
+
+    projects = ddict(list)
+
+    # group rows by net_id
+    for project_id in sys.argv[1:]:
+        with open('grades/'+project_id+'.json') as f:
+            for row in json.load(f):
+                projects[row['net_id']].append(row)
+
+    # status by net_id
+    net_ids = TEST_NET_IDS if len(TEST_NET_IDS) > 0 else projects.keys()
+    emails = []
+    now = datetime.datetime.now()
+
+    for net_id in net_ids:
+        prows = projects[net_id]
+        prows.sort(key=lambda row: int(row['project'][1:]))
+        html = gen_html(prows)
+
+        # distribute as email
+        email = {'to':net_id+'@wisc.edu',
+                 'subject': 'CS 220: Project Summary',
+                 'message':html, 'html':True}
+        emails.append(email)
+
+    # dump email file
+    with open('project_emails.json', 'w') as f:
+        json.dump(emails, f, indent=2, sort_keys=True)
+
+if __name__ == '__main__':
+    main()
