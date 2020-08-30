f5a28996ae4deab87b7da0709d76dbaef2230ac9
tylerharter <tylerharter@gmail.com>
2020-08-30 11:00:31 -0500

    Joseph fixes

--- tools/assign-crs.py
+++ tools/assign-crs.py
@@ -5,7 +5,7 @@ from collections import defaultdict as ddict
 #
 # aws s3 cp p2-cr-assignments.json s3://caraza-harter-cs301/a/projects/p2-cr-assignments.json
 
-COURSE = 'a'
+COURSE = 'b'
 
 # we want submissions that are the latest for a student
 def get_submission_emails(project):
