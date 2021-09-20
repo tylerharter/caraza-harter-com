9275f8144fb9a96311651ccdc5cec688d78904a4
Meenakshi Syamkumar <ms@cs.wisc.edu>
2021-09-20 10:46:55 -0500

    Adding template for lec 06

--- tyler/meena/cs220/f21/materials/lectureDemo_code/lec-06/add.py
+++
@@ -1,9 +0,0 @@
-x = input("Enter x: ")
-y = input("Enter y: ")
-
-#Type casting string to float
-x = float(x)
-y = float(y)
-
-print("Debug:", type(x), type(y))
-print("The sum is: ", x + y)
--- tyler/meena/cs220/f21/materials/lectureDemo_code/lec-06/parrot.py
+++
@@ -1,2 +0,0 @@
-words = input("Say something: ")
-print("Parrot says: ", words)
