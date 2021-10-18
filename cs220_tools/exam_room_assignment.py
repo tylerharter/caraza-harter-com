ad1811a2cf5a9d5df1a2236f88e227ea294ab293
Meenakshi Syamkumar <ms@cs.wisc.edu>
2021-10-18 08:07:57 -0500

    Releasing lec18 slides

--- cs220_tools/exam_room_assignment.py
+++ cs220_tools/exam_room_assignment.py
@@ -1,6 +1,14 @@
 import json
 import sys
 
+################################################################################
+# roster.json is expected to be in ../tools/
+# Writes updated roster to ../tools/exam<number>_roster.json
+# Check that and replace roster.json with that version
+# TODO: Before exam2 update this script to directly handle alternate and
+# McBurney exams
+################################################################################
+
 def read_json(path):
     with open(path, encoding="utf-8") as f:
         return json.load(f)
@@ -16,32 +24,33 @@ section3_count = 0
 section4_count = 0
 section5_count = 0
 
+locations = {
+    1: "Psychology 105; 7:15 to 9:15 pm on Friday, October 8th",
+    2: "Bascom 272; 7:15 to 9:15 pm on Friday, October 8th",
+    3: "Humanities 3650; 7:15 to 9:15 pm on Friday, October 8th",
+    4: "Ingraham B10; 7:15 to 9:15 pm on Friday, October 8th",
+    5: "Online Honorlock exam; 24-hour window period from 3:00 PM (CST) on Friday, October 8th"
+}
+
+section_counts = {}
+
+if len(sys.argv) < 2:
+    print("Usage: python3 {} <exam name, ex: exam1>".format(sys.argv[0]))
+    sys.exit(1)
 
 exam_name = sys.argv[1]
 
 for student_dict in roster:
     if not student_dict['enrolled']:
         continue
-    if(student_dict['section']) == 1:
-        student_dict[exam_name] = "Psychology 105; 7:15 to 9:15 pm on Friday, October 8th"
-        section1_count += 1        
-    elif(student_dict['section']) == 2:
-        student_dict[exam_name] = "Bascom 272; 7:15 to 9:15 pm on Friday, October 8th"
-        section2_count += 1        
-    elif(student_dict['section']) == 3:
-        student_dict[exam_name] = "Humanities 3650; 7:15 to 9:15 pm on Friday, October 8th"
-        section3_count += 1        
-    elif(student_dict['section']) == 4:
-        student_dict[exam_name] = "Ingraham B10; 7:15 to 9:15 pm on Friday, October 8th"
-        section4_count += 1        
-    elif(student_dict['section']) == 5:
-        student_dict[exam_name] = "Online Honorlock exam; 24-hour window period from 3:00 PM (CST) on Friday, October 8th"
-        section5_count += 1        
+    if student_dict['section'] not in section_counts:
+        section_counts[student_dict['section']] = 0
+    else:
+        section_counts[student_dict['section']] += 1
+    
+    student_dict[exam_name] = locations[student_dict['section']]
 
-print("SEC001", section1_count)
-print("SEC002", section2_count)
-print("SEC003", section3_count)
-print("SEC004", section4_count)
-print("SEC005", section5_count)
+for section in section_counts:
+    print("SEC00{} student count: {}".format(section, section_counts[section]))
 
-write_json("../tools/exam1_roster.json", roster)
+write_json("../tools/{}_roster.json".format(exam_name), roster)
